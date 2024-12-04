57c7fed710c99ebd947710a6294c00b980043c4a
Kyungnan Kim <kht241022@gmail.com>
2024-12-04 11:01:44 +0900

    12-04 interface

--- JAVAPROJECT/src/com/kh/practiceTotal/BankAccountRun.java
+++ JAVAPROJECT/src/com/kh/practiceTotal/BankAccountRun.java
@@ -1,4 +1,11 @@
 package com.kh.practiceTotal;
 
 public class BankAccountRun {
+    public static void main(String[] args) {
+        //                                                        계좌번호,     현재잔액
+        BankAccount bankAccount = new BankAccount("123-456",1000);
+        bankAccount.deposit(10000);
+        bankAccount.withdraw(5000);
+        bankAccount.withdraw(15000000);
+    }
 }
